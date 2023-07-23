2acb46899cd3c898a2e38d192158f59710c6da21
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2023-07-22 21:47:35 -0300

    testing

--- libtcalc/tcalc_export.h
+++ libtcalc/tcalc_export.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#pragma warning (disable: 4251)
-// Please make sure you're building this library with a compatible ABI with the rest of your program!
-
 #ifdef _WIN32
     #ifdef TCALC_LIBRARY
         #define TCALC_EXPORT __declspec(dllexport)
--- tcalc_console/main.cpp
+++ tcalc_console/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include "string_reader.h"
+
+void printToken(TCalc::SourceToken);
 
 int main()
 {
     std::cout << "tcalc console" << std::endl;
+    std::string input;
+    std::string aux;
+    while (true)
+    {
+        std::cout << "> ";
+        std::getline(std::cin, aux);
+        if (aux.empty())
+            break;
+        else
+            input += aux + "\n";
+    }
+    std::cout << "input:\n" << input;
+
+    TCalc::StringReader sr(input);
+    sr.moveNextCharacter();
+    auto token = sr.flushToken();
+    printToken(token);
+}
+
+void printToken(TCalc::SourceToken token)
+{
+    std::cout << "source token information:\n" << token.getString() << std::endl;
+    std::cout << "start -- line: " << token.getStart().line << "  column: " << token.getStart().column;
+    std::cout << "\nend -- line: " << token.getEnd().line << "  column: " << token.getEnd().column;
+
 }
