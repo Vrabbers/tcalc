316b60beda2fcf215ef1f2cead602f7ebe204438
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2024-04-23 00:03:58 -0300

    weird warning

--- libtcalc/utf8_utils.h
+++ libtcalc/utf8_utils.h
@@ -7,7 +7,7 @@
 // Wrappers around utf8proc functions to make other code more readable
 namespace utf8proc
 {
-    static std::string encode_char(const char32_t character)
+    inline std::string encode_char(const char32_t character)
     {
         char buf[4] = { };
         const auto count = utf8proc_encode_char(static_cast<int32_t>(character), reinterpret_cast<uint8_t*>(buf));
@@ -15,13 +15,13 @@ namespace utf8proc
     }
 
     // WARNING! Make sure index < string.size()!
-    static std::ptrdiff_t iterate_one_from_index(const std::string& string, const std::size_t index, char32_t* character)
+    inline std::ptrdiff_t iterate_one_from_index(const std::string& string, const std::size_t index, char32_t* character)
     {
         const auto start_ptr = reinterpret_cast<const std::uint8_t*>(&string.c_str()[index]);
         return utf8proc_iterate(start_ptr, -1, reinterpret_cast<int32_t*>(character));
     }
 
-    static utf8proc_category_t category(char32_t character)
+    inline utf8proc_category_t category(char32_t character)
     {
         return utf8proc_category(static_cast<int32_t>(character));
     }
